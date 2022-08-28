22a20d22a777c6ab6f7706bc01fd73d648b1b43e
Maksim Kovtun <brain1999@mail.ru>
2022-08-28 15:23:16 -0400

    Schema tests were added

---
+++ tests/schemas/test_dron.py
@@ -0,0 +1,10 @@
+import pytest
+from schemas.dron import Dron
+
+
+def test_check_default_values():
+    dron = Dron()
+    assert dron.coordinates == (0, 0)
+    assert dron.fuel == 20
+    assert dron.distance_to_base == 0
+    assert dron.next_coordinates == (0, 0)
